5ce16b8192ae74c728b97a900d58200b031feb23
Brian Mo <brimo2912@gmail.com>
2019-12-14 20:00:59 -0500

    Renamed util folder to `startup`
    Since the `init.sh` file will be `source`-ed in my shell rc file,
    it should really be called `startup/`.

--- util/constants.sh
+++
@@ -1,9 +0,0 @@
-# Constant variables needed for this project.
-
-__THIS_SCRIPT="$( cd "$(dirname "$0")" ; pwd -P )"
-
-UTIL_DIR="$(dirname ${__THIS_SCRIPT})";
-ROOT_DIR="$(dirname ${UTIL_DIR})";
-
-export UTIL_DIR;
-export ROOT_DIR;
