f3307feea4d708363c2fd02d7086b9a30ea306c5
Brian Mo <brimo2912@gmail.com>
2019-12-14 20:02:36 -0500

    Updated scripts to reflect delta in `startup/`

--- dev.sh
+++ dev.sh
@@ -7,6 +7,14 @@
 # Dev CLI Tool for managing the environment + providing helpful shell utility
 # functions. Compatible with most shells, but mainly `bash`, `zsh`, and `sh`.
 
+__dev_logo = "
+    ____  _______     __
+   |  _ \| ____\ \   / /
+   | | | |  _|  \ \ / / 
+   | |_| | |___  \ V /  
+   |____/|_____|  \_/   
+"
+
 function is_zsh() {
   [ -n "${ZSH_VERSION-}" ]
 }
--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 # run on shell start up.
 export project_init_status="OK";
 
-__THIS_DIR="$( cd "$(dirname "$0")" ; pwd -P )";
-source "${__THIS_DIR}/util/constants.sh";
-source "${__THIS_DIR}/util/portable.sh";
-source "${__THIS_DIR}/util/import.sh";
+localvar_THIS_DIR="$( cd "$(dirname "$0")" ; pwd -P )";
+source "${localvar_THIS_DIR}/startup/constants.sh";
+source "${localvar_THIS_DIR}/startup/portable.sh";
+source "${localvar_THIS_DIR}/startup/module.sh";
